405139204bcb01c96d830ee5cbe8e2616a864c5b
sidaliSadi <sidalisadi230@gmail.com>
2022-10-18 14:19:33 +0200

    add_lower_case function

--- src/check_pubmed.py
+++ src/check_pubmed.py
@@ -5,7 +5,9 @@ from common import init_spark
 spark = init_spark("discover", driver_memory=4)
 
 #load data
-def load_original_data(file_path):
+def load_original_data(
+    file_path
+    ):
     return spark.read.csv(
         file_path,
         encoding='utf-8',
@@ -13,3 +15,9 @@ def load_original_data(file_path):
         header=True,
         inferSchema=True
     )
+
+#lower case
+def lower_case(df, col):
+    return df.withColumn(col+'_', F.lower(F.col(col)))\
+    .drop(col)
+
