2b2d9d56de7481f7ed0dbf1acd9abbbe70f7641d
sidaliSadi <sidalisadi230@gmail.com>
2022-10-18 10:18:01 +0200

    add_load_data function

---
+++ src/check_pubmed.py
@@ -0,0 +1,15 @@
+import pyspark.sql.functions as F
+from common import init_spark
+
+
+spark = init_spark("discover", driver_memory=4)
+
+#load data
+def load_original_data(file_path):
+    return spark.read.csv(
+        file_path,
+        encoding='utf-8',
+        mode='FAILFAST',
+        header=True,
+        inferSchema=True
+    )
