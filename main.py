fbc56ede11cf9db1f04e80d8cd1b540733b47f40
sidaliSadi <sidalisadi230@gmail.com>
2022-10-19 18:22:07 +0200

    call newspaper_with_most_different_drugs from main file

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 
 from src.graph import Node
 from src.spark_pipline import read_files, process_df, save_as_tree
+from src.ad_hoc import newspaper_with_most_different_drugs
 
 if __name__ == '__main__':
     root = Node('root')
@@ -11,12 +12,14 @@ if __name__ == '__main__':
     'clinical_trials': '/home/sadi/Bureau/esmoz_servier/esmoz_servier/data/clinical_trials.csv'
 }
 
-    #read files
-    print("reading files ..")
-    dict_dfs = read_files(dict_pattern)
+    # #read files
+    # print("reading files ..")
+    # dict_dfs = read_files(dict_pattern)
 
-    # apply process
-    process_df(dict_dfs)
+    # # apply process
+    # process_df(dict_dfs)
 
-    root = Node('root')
-    save_as_tree(root, 'result/final_result.csv')
+    # root = Node('root')
+    # save_as_tree(root, 'result/final_result.csv')
+
+    print(f"Le journal avec le plus de medicaments differents est: {newspaper_with_most_different_drugs('result/final_result.json')}")
