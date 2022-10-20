3412856c758ccfc034df4ae42db7c9748903fea1
sidaliSadi <sidalisadi230@gmail.com>
2022-10-20 11:14:11 +0200

    update pths in dict_pattern

--- main.py
+++ main.py
@@ -1,27 +1,30 @@
 
+from unittest import result
 from src.graph import Node
 from src.spark_pipline import read_files, process_df, save_as_tree
 from src.ad_hoc import newspaper_with_most_different_drugs
 
 if __name__ == '__main__':
     root = Node('root')
-
+    
     dict_pattern = {
-    'drugs': '/home/sadi/Bureau/esmoz_servier/esmoz_servier/data/drugs.csv',
-    'pubmed': '/home/sadi/Bureau/esmoz_servier/esmoz_servier/data/pubmed.csv',
-    'clinical_trials': '/home/sadi/Bureau/esmoz_servier/esmoz_servier/data/clinical_trials.csv'
+    'drugs': 'esmoz_servier/data/drugs.csv',
+    'pubmed': 'esmoz_servier/esmoz_servier/data/pubmed.csv',
+    'clinical_trials': 'esmoz_servier/esmoz_servier/data/clinical_trials.csv'
 }
 
     #read files
     print("reading files ..")
     dict_dfs = read_files(dict_pattern)
 
-    # apply process
+    # apply 
+    print('processing the dataframes !!')
     process_df(dict_dfs)
 
-    root = Node('root')
+    print('saving result as json file ')
     save_as_tree(root, 'result/final_result.csv')
 
+    
     print(f"Le journal avec le plus de medicaments differents est: {newspaper_with_most_different_drugs('result/final_result.json')}")
 
  
